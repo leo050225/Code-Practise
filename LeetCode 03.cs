7f0988592f5cdcc890795b5a5be44a8b08424fb2
leo050225 <leo050225@gmail.com>
2024-11-24 16:25:04 +0800

    Create LeetCode 03.cs

---
+++ LeetCode 03.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) 
+    {
+        HashSet<char> letter = new HashSet<char>();
+        int left = 0,right = 0;
+        int length = s.Length;
+        int count = 0,max = 0;
+
+        while(right<length)
+        {
+            if(!letter.Contains(s[right]))
+            {
+                letter.Add(s[right]);
+                right++;
+                count++;
+            }
+            else
+            {
+                letter.Remove(s[left]);
+                left++;
+                count--;
+            }
+            max = Math.Max(max,count);
+        }
+        return max;
+    }
+}
